03b8824894ac993df60f24fceb3f16e87709e671
Christian Navolskyi <c.navolskyi@gmail.com>
2021-10-10 17:53:29 +0200

    extruder working, hot bed missing

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -711,7 +711,7 @@
  * *** IT IS HIGHLY RECOMMENDED TO LEAVE THIS OPTION ENABLED! ***
  */
 #define PREVENT_COLD_EXTRUSION
-#define EXTRUDE_MINTEMP 170
+#define EXTRUDE_MINTEMP 50
 
 /**
  * Prevent a single extrusion longer than EXTRUDE_MAXLENGTH.
@@ -1332,7 +1332,7 @@
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-#define INVERT_E0_DIR false
+#define INVERT_E0_DIR true
 #define INVERT_E1_DIR false
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
--- Marlin/src/pins/lpc1768/pins_BTT_SKR_V1_3.h
+++ Marlin/src/pins/lpc1768/pins_BTT_SKR_V1_3.h
@@ -188,11 +188,11 @@
   #define Z_SERIAL_TX_PIN                  P1_14
   #define Z_SERIAL_RX_PIN                  P1_10
 
-  #define E0_SERIAL_TX_PIN                 P1_09
-  #define E0_SERIAL_RX_PIN                 P1_08
+  #define Z2_SERIAL_TX_PIN                 P1_09
+  #define Z2_SERIAL_RX_PIN                 P1_08
 
-  #define E1_SERIAL_TX_PIN                 P1_04
-  #define E1_SERIAL_RX_PIN                 P1_01
+  #define E0_SERIAL_TX_PIN                 P1_04
+  #define E0_SERIAL_RX_PIN                 P1_01
 
   // Reduce baud rate to improve software serial reliability
   #define TMC_BAUD_RATE                    19200
